f4611a1ab9b7db4955d04ec5a4b75c19394b796e
S-Yamamori <mori0124saori@gmail.com>
2013-10-07 17:14:10 +0900

    シーン遷移のプロジェクト全体をぶち込みました

---
+++ Assets/ResultScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// scene change
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Application.LoadLevel("title");
+		}
+	
+		// scene change
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Application.LoadLevel("game");
+		}
+
+	}
+}
---
+++ Assets/TitleScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// scene change
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Application.LoadLevel("game");
+		}
+		
+		// scene change
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Application.LoadLevel("result");
+		}
+
+	}
+}
