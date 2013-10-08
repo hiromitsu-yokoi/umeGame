9996fb1754b2fed08ac6970e4e00bc3ec1b4b919
kobayashi hiroshi <zingaimakiyou@gmail.com>
2013-10-08 11:08:49 +0900

    操作の調節

--- Assets/field.cs
+++ Assets/field.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 using System.Collections;
 
 public class field : MonoBehaviour {
-
+	
+	static float fSlope; 
+	
 	// Use this for initialization
 	void Start () {
-	
+		
+		fSlope = 25.4f;
 	}
 	
 	// Update is called once per frame
@@ -13,28 +16,33 @@ public class field : MonoBehaviour {
 
 		Vector3 VecRoll;
 		Vector3 Roll;
-		Quaternion FiledRoll;
-		FiledRoll = transform.rotation;
 		VecRoll = Input.acceleration;
-		Roll.x = VecRoll.x * 12.8f;
+/*
+		Roll.x = VecRoll.y;
+		Roll.y = 0.0f;
+		Roll.z = -VecRoll.x;
+		
+		transform.Rotate(Roll);
+*/		
+		Roll.x = VecRoll.x * fSlope;
 		Roll.y = -9.8f;
-		Roll.z = VecRoll.y * 12.8f;
+		Roll.z = (VecRoll.y + 0.7f ) * fSlope;
 		
 		if(Input.GetKey(KeyCode.A))
 		{
-			Roll.x = -12.8f;			
+			Roll.x = -fSlope;			
 		}
 		if(Input.GetKey(KeyCode.D))
 		{
-			Roll.x = 12.8f;						
+			Roll.x = fSlope;						
 		}
 		if(Input.GetKey(KeyCode.S))
 		{
-			Roll.z = -12.8f;		
+			Roll.z = -fSlope;		
 		}
 		if(Input.GetKey(KeyCode.W))
 		{
-			Roll.z = 12.8f;		
+			Roll.z = fSlope;		
 		}
 		
 		Physics.gravity = Roll;
